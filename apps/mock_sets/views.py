3d3476f46696ada39fc3d802beb842922a03d37c
carol-muchemi <carolm@adalabsafrica.com>
2021-02-22 22:44:02 +0300

    initial commit with models, serializers, views and urls defined

---
+++ apps/mock_sets/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+
+from rest_framework import (viewsets, mixins)
+
+from apps.mock_sets.models import (Endpoint, MLAlgorithm, MLAlgorithmStatus, MLRequest,)
+from apps.mock_sets.serializers import (
+    EndpointSerializer, MLAlgorithmSerializer, MLAlgorithmStatusSerializer, MLRequestSerializer
+    )
+
+# Create your views here.
+class EndpointViewSet(
+    mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+):
+    serializer_class = EndpointSerializer
+    queryset = Endpoint.objects.all()
+
+class MLAlgorithmViewSet(
+    mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+):
+    serializer_class = MLAlgorithmSerializer
+    queryset = MLAlgorithm.objects.all()
+
+def deactivate_other_statuses(instance):
+    old_statuses = MLAlgorithmStatus.objects.filter(parent_mlalgorithm=instance.parent_mlalgorithm,
+                                                    created_at__lt=instance.created_at,
+                                                    active=True)
+    for i in range(len(old_statuses)):
+        old_statuses[i].active = False
+    MLAlgorithmStatus.objects.bulk_update(old_statuses, ["active"])
+
+class MLAlgorithmStatusViewSet(
+    mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+):
+    serializer_class = MLAlgorithmStatusSerializer
+    queryset = MLAlgorithmStatus.objects.all()
+    def perform_create(self, serializer):
+        try:
+            with transaction.atomic():
+                instance = serializer.save(active=True)
+                deactivate_other_statuses(instance)
+        except Exception as e:
+            raise APIException(str(e))
+
+class MLRequestViewSet(
+    mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet, mixins.UpdateModelMixin
+):
+    serializer_class = MLRequest
+    queryset = MLRequest.objects.all()
