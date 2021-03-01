d11239e725b1003b7739e6b60157181ac1ca932c
carol-muchemi <carolm@adalabsafrica.com>
2021-03-01 22:56:31 +0300

    rf: test RandomForest class

--- apps/machine_learning_models/tests/tests.py
+++ apps/machine_learning_models/tests/tests.py
@@ -1,16 +1,16 @@
 from django.test import TestCase
 
-from apps.machine_learning_models.classifier.logistic_regression import LogisticRegression
+from apps.machine_learning_models.classifier.random_forest import RandomForest
 
 class MLTests(TestCase):
-    def test_lr_algorithm(self):
+    def test_rf_algorithm(self):
         data = {
-            "Short Desc": "Metallic Liquidtight Conduit, Flexible, LA, Galvanized Steel (Inner Core), PVC (Jacket)",
-            "Long Desc": "_x000D_Type LA_x000D_",
-            "Size": "2-1/2 in.",
-            "Length": "100 ft.",
-            "Material": "PVC Coated Galvanized Steel"
+            "Short Desc": "Steel Conduit Hot-Dippedâ Galvanizedâ Steel (Inner Core); PVC (Liquidtightâ Jacket), 1/2 in.",
+            "Long Desc": "1/2 in. PVC-coated galvanized steel type ATLA grey liquid-tight conduit. Conduit is 1000 ft."
+            # "Short Desc": "Metallic Liquidtight Conduit, Flexible, LA, Galvanized Steel (Inner Core), PVC (Jacket)",
+            # "Long Desc": "_x005F_x000D_Type LA_x005F_x000D_",
         }
-        algorithm = LogisticRegression()
-        response = algorithm.preprocessing(data)
+        algorithm = RandomForest()
+        response = algorithm.compute_prediction(data)
         self.assertEqual("OK", response["status"])
+        self.assertTrue("label" in response)
