ff0d91b00956c6d8ed310e9a8c5fda6d3616906e
carol-muchemi <carolm@adalabsafrica.com>
2021-03-03 12:49:14 +0300

    Create registry file

---
+++ apps/machine_learning_models/classifier/registry.py
@@ -0,0 +1,11 @@
+from apps.mock_sets.models import Endpoint
+from apps.mock_sets.models import MLAlgorithm
+from apps.mock_sets.models import MLAlgorithmStatus
+
+class MLRegistry:
+    def __init__(self):
+        self.endpoints = {}
+
+    def add_algorithm(
+            self, endpoint_name, algorithm_object, algorithm_name, algorithm_status, algoritm_version, owner):
+            
