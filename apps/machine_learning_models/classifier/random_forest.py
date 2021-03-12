2e3230b61e0df51d92370cd20132d8d19575f7bc
carol-muchemi <carolm@adalabsafrica.com>
2021-03-12 22:53:42 +0300

    rf: edit models with format and automate encoded variables

--- apps/machine_learning_models/classifier/random_forest.py
+++ apps/machine_learning_models/classifier/random_forest.py
@@ -1,3 +1,4 @@
+import ast
 import re
 import string
 
@@ -12,13 +13,28 @@ class RandomForest:
         # self.data = pd.read_excel(
         #     "/home/wambui/Fiverr/Python/notebooks/sanveohr/mockdata_set.xlsx",
         #     sheet_name="input_1_conduit_data")
-        path = ("/home/wambui/Fiverr/Python/notebooks/sanveohr/")
+        path = ("/home/wambui/Fiverr/Python/notebooks/sanveohr/{}")
         self.material_model = joblib.load(
-                                    path + "material_random_forest.joblib")
-        self.length_model = joblib.load(path + "length_random_forest.joblib")
-        self.type_model = joblib.load(path + "type_random_forest.joblib")
-        self.size_model = joblib.load(path + "/size_random_forest.joblib")
-        self.empty_dataframe = pd.read_excel("/home/wambui/Fiverr/Python/notebooks/sanveohr/empty_dataframe.xlsx", )
+                                    path.format(
+                                        "material_random_forest.joblib"))
+        self.length_model = joblib.load(
+                                    path.format("length_random_forest.joblib"))
+        self.type_model = joblib.load(
+                                    path.format("type_random_forest.joblib"))
+        self.size_model = joblib.load(
+                                    path.format("/size_random_forest.joblib"))
+        self.empty_dataframe = pd.read_excel(
+                                    path.format("empty_dataframe.xlsx"))
+
+        try:
+            with open(path.format("type_.txt"), "r") as type_, open(path.format("material.txt"), "r") as material, open(path.format("length.txt"), "r") as length, open(path.format("size.txt"), "r") as size:
+                self.type_ = type_.read()
+                self.material = material.read()
+                self.length = length.read()
+                self.size = size.read()
+
+        except Exception as e:
+            return {"status": "Error", "message": str(e)}
 
     def preprocessing(self, data):
         """
@@ -101,39 +117,20 @@ class RandomForest:
         """
         Decode the encoded outputs for consumption by users
         """
-        available_materials = {
-            23: 'PVC Coated Galvanized Steel', 35: 'stainless steel', 
-            21: 'PVC', 18: 'Nylon', 8: 'Galvanized Steel', 
-            33: 'Stainless Steel', 3: 'Aluminium', 26: 'Plenum-PVDF', 
-            27: 'Riser-Nylon', 28: 'Riser-PVDF', 11: 'HDPE-Schedule 80', 
-            10: 'HDPE', 31: 'Schedule 80', 29: 'Schedule 40', 
-            6: 'Corrosion Resistant Plated Steel, PVC', 
-            13: 'Hot Dipped Galvanized Steel, PVC', 
-            15: 'Hot-Dippedâ Galvanizedâ Steel (Inner Core); PVC', 
-            9: 'Galvanized Steel, Thermoplastic PVC (Jacket)', 
-            22: 'PVC (Jacket), Plated Steel (Inner Core)', 
-            24: 'PVC-Coated Galvanized Steel', 16: 'Non-Metallic', 
-            5: 'Carbon Strip Steel', 7: 'Electroplated Steel', 
-            14: 'Hot Galvanized Steel', 2: '316 Stainless Steel', 
-            34: 'Steel', 4: 'Aluminum', 1: '304 Stainless Steel', 
-            12: 'High Density Polyethylene', 0: '0', 19: 'Nylon Resin', 
-            30: 'Schedule 40 PVC', 32: 'Schedule 80 PVC', 
-            20: 'Nylon, Polyamide', 17: 'Non-Metallic/PVC', 25: 'PVDF'
-            }
+        available_materials = self.material
+        available_materials = ast.literal_eval(available_materials)
+
         for pred in available_materials:
             if prediction == pred:
                 material = available_materials[pred]
 
-        return {"prediction": prediction, "label": material, "status": "OK"}
+        return print({"prediction": prediction, "label": material, "status": "OK"})
 
     def size_postprocessing(self, prediction):
 
-        available_sizes = {
-            7: '2-1/2 in.', 6: '2 in.', 5: '1/2 in.', 11: '3/4 in.', 
-            3: '1-1/2 in.', 9: '3 in.', 4: '1-1/4 in.', 2: '1 in.', 
-            13: '4 in.', 10: '3-1/2 in.', 14: '5 in.', 15: '6 in.', 
-            1: '1 1/4 in.', 8: '21 mm', 12: '4', 0: '0'
-            }
+        available_sizes = self.size
+        available_sizes = ast.literal_eval(available_sizes)
+
         for pred in available_sizes:
             if prediction == pred:
                 size = available_sizes[pred]
@@ -141,28 +138,19 @@ class RandomForest:
         return print({"prediction": prediction, "label": size, "status": "OK"})
 
     def length_postprocessing(self, prediction):
-        available_lengths = {
-            4: '100 ft.', 16: '25 ft.', 5: '1000 ft.', 26: '50 ft.', 
-            37: 'Cut Reel', 27: '500 ft.', 12: '200 ft.', 2: '10 ft.', 
-            11: '20 ft.', 25: '400 ft.', 15: '25 Ft.', 21: '3 ft.', 
-            35: '8 ft.', 22: '3.50 ft.', 9: '15 ft.', 33: '750 ft.', 
-            17: '250 ft.', 36: '8000 ft.', 32: '700 ft.', 8: '1400 ft.', 
-            31: '6500 ft.', 28: '5000 ft.', 34: '7500 ft.', 18: '2500 ft.', 23: '3000 ft.', 29: '600 ft.', 19: '2700 ft.', 3: '10,000 ft.', 
-            0: '0', 6: '118.5 in.', 7: '119 in.', 1: '10 Ft.', 20: '2730 ft.', 30: '6100 ft.', 10: '20 Ft.', 38: 'Multiple', 24: '350 ft.', 
-            14: '225 ft.', 13: '2000 ft.'
-            }
+        available_lengths = self.length
+        available_lengths = ast.literal_eval(available_lengths)
+
         for pred in available_lengths:
-            if prediction == pred:
+            if prediction == int(pred):
                 length = available_lengths[pred]
 
         return print({"prediction": prediction, "label": length, "status": "OK"})
 
     def type_postprocessing(self, prediction):
-        available_types = {
-            15: 'LFMC', 16: 'LFNC', 4: 'EMT', 6: 'FMC', 11: 'GRC', 13: 'IMC', 20: 'PVCC', 21: 'RMC', 5: 'ENT', 7: 'FNC', 14: 'Innerduct', 
-            19: 'PVC', 10: 'Flexible, Liquidtight', 8: 'Flexible', 1: 'ATLA', 18: 'Liquidtight Flexible', 0: '0', 9: 'Flexible Metallic', 
-            17: 'LL', 23: 'Smoothwall', 12: 'HDPE Conduit', 2: 'Corrugated', 3: 'Corrugated HDPE', 22: 'Rise Raceway'
-            }
+        available_types = self.type_
+        available_types = ast.literal_eval(available_types)
+
         for pred in available_types:
             if prediction == pred:
                 types = available_types[pred]
@@ -194,7 +182,8 @@ class RandomForest:
 
         return material_predicted_output, size_predicted_output, length_predicted_output, type_predicted_output
 
-RandomForest().compute_prediction(data = {
-            "Short Desc": "Steel Conduit Hot-Dippedâ Galvanizedâ Steel (Inner Core); PVC (Liquidtightâ Jacket), 1/2 in.",
-            "Long Desc": "1/2 in. PVC-coated galvanized steel type ATLA grey liquid-tight conduit. Conduit is 1000 ft."
-        })
+# RandomForest().compute_prediction(data = {
+#             "Short Desc": "PolyTuff® I Non-Metallic Grey Liquidtight Flexible Conduit, 1-1/4 in.",
+#             "Long Desc": "_x000D_1-1/4 in. PolyTuff® I grey non-metallic liquidtight flexible conduit, length 100 ft._x000D_\
+#             Hubbell Polytuff I Conduit and Polytuff II Tubing are entirely non-metallic, providing superior flexibility and outstanding corrosion resistance._x000D_"
+#         })
